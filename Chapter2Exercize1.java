39c386d6bc9f9def1a6711800bf776af57c56778
Kirill <kirill.motrichkin@ya.ru>
2018-12-14 23:55:08 +0300

    First task.

---
+++ Chapter2Exercize1.java
@@ -0,0 +1,8 @@
+public class Chapter2Exercize1 {
+	static int i;
+	static char c;
+	public static void main(String[] args) {
+		System.out.printf("The int is %d%n", i);
+		System.out.printf("The char is '%s'%n", c);
+	}
+}
