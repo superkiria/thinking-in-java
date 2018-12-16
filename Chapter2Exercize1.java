9b3982c9807075f6280d9c8d83ad9ea87bf0c35c
superkiria <41254237+superkiria@users.noreply.github.com>
2018-12-16 19:49:47 +0300

    Delete Chapter2Exercize1.java

--- Chapter2Exercize1.java
+++
@@ -1,8 +0,0 @@
-public class Chapter2Exercize1 {
-	static int i;
-	static char c;
-	public static void main(String[] args) {
-		System.out.printf("The int is %d%n", i);
-		System.out.printf("The char is '%s'%n", c);
-	}
-}
